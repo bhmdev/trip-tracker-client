d54a8ac8cd3a9b27231d33326b97a41dd86b3d71
bhmdev <bmontima@gmail.com>
2020-03-31 16:44:28 -0400

    Nav Bar styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import styled from 'styled-components'
 
 const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
     <Nav.Link href="#create-trip">Add Trip</Nav.Link>
-
   </Fragment>
 )
 
@@ -24,20 +24,29 @@ const alwaysOptions = (
   </Fragment>
 )
 
+const SecondNav = styled.section`
+  padding: 100px;
+  margin: auto;
+  background: #71b9ff;
+`
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
-    <Navbar.Brand href="#">
-      trip_tracker
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-        { alwaysOptions }
-        { user ? authenticatedOptions : unauthenticatedOptions }
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <div>
+    <Navbar bg="info" variant="light" expand="md">
+      <Navbar.Brand href="#">
+        Trip Tracker
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+          { alwaysOptions }
+          { user ? authenticatedOptions : unauthenticatedOptions }
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <SecondNav>
+    </SecondNav>
+  </div>
 )
 
 export default Header
